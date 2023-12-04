0e73c8c83a35228c3b3e0de029c7722f897bd243
DimasAliA <143428324+DimasAliA@users.noreply.github.com>
2023-12-05 01:00:56 +0800

    Update loginpage.jsx

--- src/components/loginpage/loginpage.jsx
+++ src/components/loginpage/loginpage.jsx
@@ -3,8 +3,6 @@
 import React from 'react';
 // import Login from './loginform';
 import { Header, MainContent, Footer } from './backgroundlogin'; // Sesuaikan path
-import './login.css';
-import './bglogin.css'
 
 const LoginPage = () => {
     return (
