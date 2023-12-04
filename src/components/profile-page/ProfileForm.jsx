d365eb7681cc592aa83794b65a56a53261b84e14
DimasAliA <143428324+DimasAliA@users.noreply.github.com>
2023-12-04 23:48:33 +0800

    Update ProfileForm.jsx

--- src/components/profile-page/ProfileForm.jsx
+++ src/components/profile-page/ProfileForm.jsx
@@ -1,109 +1,129 @@
 import React, { useState, useEffect } from 'react';
 import './profile.css';
 
-function ProfileForm({ profileImage,setProfileImage }) {
-  const [profile, setProfile] = useState({
-    firstName: '',
-    lastName: '',
-    about: '',
-    username: '',
-  });
+function ProfileForm({ profileImage, setProfileImage }) {
+    const [profile, setProfile] = useState({
+        firstName: '',
+        lastName: '',
+        about: '',
+        username: '',
+    });
 
-  const [isEditing, setIsEditing] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
 
-  useEffect(() => {
-    const storedProfile = localStorage.getItem('profile');
-    if (storedProfile) {
-      setProfile(JSON.parse(storedProfile));
-    }
-  }, []);
+    useEffect(() => {
+      const currentUsername = localStorage.getItem('currentUsername');
+      if (currentUsername) {
+        const registeredUsers = JSON.parse(localStorage.getItem('registeredUsers')) || [];
+        const userData = registeredUsers.find(user => user.username === currentUsername);
+        if (userData) {
+          setProfile({
+            firstName: userData.fullName.split(' ')[0] || '',
+            lastName: userData.fullName.split(' ')[1] || '',
+            about: '',
+            username: userData.username,
+          });
+          if (userData.profileImage) {
+            setProfileImage(userData.profileImage);
+          }
+        }
+      }
+    }, []);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setProfile({ ...profile, [name]: value });
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProfile({ ...profile, [name]: value });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    localStorage.setItem('profile', JSON.stringify(profile));
-    setIsEditing(false);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.setItem('profile', JSON.stringify(profile));
+        setIsEditing(false);
+    };
 
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
+    const handleEdit = () => {
+        setIsEditing(true);
+    };
 
-  const handleImageChange = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      let img = e.target.files[0];
-      setProfileImage(URL.createObjectURL(img));
-    }
-  };
+    const handleImageChange = (e) => {
+        if (e.target.files && e.target.files[0]) {
+            let reader = new FileReader();
 
-  return (
-    <div className="profile-card">
-      <div className="profile-image-section">
-        <img src={profileImage} alt="Profil" className="profile-image"/>
-        <input type="file" onChange={handleImageChange} hidden id="fileInput" />
-        <button type="button" className="change-picture-button" onClick={() => document.getElementById('fileInput').click()}>Ganti</button>
+            reader.onload = function (e) {
+                setProfileImage(e.target.result);
+                localStorage.setItem('profileImage', e.target.result);
+            };
+
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    };
+
+    return (
+        <div className="profile-card">
+            <div className="profile-image-section">
+                <img src={profileImage} alt="Profil" className="profile-image" />
+                <input type="file" onChange={handleImageChange} hidden id="fileInput" />
+                <button type="button" className="change-picture-button" onClick={() => document.getElementById('fileInput').click()}>
+                    Ganti
+                </button>
+            </div>
+            <div className="profile-form-section">
+              <form onSubmit={handleSubmit} className="profile-form">
+                <div className="form-row">
+                  <div className="input-group">
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                      type="text"
+                      name="firstName"
+                      placeholder="Nama depan"
+                      value={profile.firstName}
+                      onChange={handleChange}
+                      disabled={!isEditing}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                      type="text"
+                      name="lastName"
+                      placeholder="Nama belakang"
+                      value={profile.lastName}
+                      onChange={handleChange}
+                      disabled={!isEditing}
+                    />
+                  </div>
+                  <div className="input-group full-width">
+                    <label htmlFor="about">Tentang</label>
+                    <textarea
+                      name="about"
+                      placeholder="Tentang"
+                      value={profile.about}
+                      onChange={handleChange}
+                      disabled={!isEditing}
+                    />
+                  </div>
+                  <div className="input-group full-width">
+                    <label htmlFor="username">Nama Pengguna</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="Nama pengguna"
+                      value={profile.username}
+                      onChange={handleChange}
+                      disabled={!isEditing}
+                    />
+                  </div>
+                </div>
+                <button 
+                  className={`edit-btn ${isEditing ? 'edit-btn-save' : 'edit-btn-edit'}`}
+                  type="button" 
+                  onClick={isEditing ? handleSubmit : handleEdit}>
+                  {isEditing ? 'Simpan' : 'Edit'}
+              </button>
+              </form>
+            </div>
       </div>
-      <div className="profile-form-section">
-        <form onSubmit={handleSubmit} className="profile-form">
-          <div className="form-row">
-            <div className="input-group">
-              <label htmlFor="firstName">First Name</label>
-              <input
-                type="text"
-                name="firstName"
-                placeholder="Nama depan"
-                value={profile.firstName}
-                onChange={handleChange}
-                disabled={!isEditing}
-              />
-            </div>
-            <div className="input-group">
-              <label htmlFor="lastName">Last Name</label>
-              <input
-                type="text"
-                name="lastName"
-                placeholder="Nama belakang"
-                value={profile.lastName}
-                onChange={handleChange}
-                disabled={!isEditing}
-              />
-            </div>
-            <div className="input-group full-width">
-              <label htmlFor="about">Tentang</label>
-              <textarea
-                name="about"
-                placeholder="Tentang"
-                value={profile.about}
-                onChange={handleChange}
-                disabled={!isEditing}
-              />
-            </div>
-            <div className="input-group full-width">
-              <label htmlFor="username">Nama Pengguna</label>
-              <input
-                type="text"
-                name="username"
-                placeholder="Nama pengguna"
-                value={profile.username}
-                onChange={handleChange}
-                disabled={!isEditing}
-              />
-            </div>
-          </div>
-          <button 
-            className={`edit-btn ${isEditing ? 'edit-btn-save' : 'edit-btn-edit'}`}
-            type="button" 
-            onClick={isEditing ? handleSubmit : handleEdit}>
-            {isEditing ? 'Simpan' : 'Edit'}
-        </button>
-        </form>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ProfileForm;
