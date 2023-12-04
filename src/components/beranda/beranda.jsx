71c85d5600ac3cfd2fccad0b13e999e8f783621d
FelixDeanJ <108813645+FelixDeanJ@users.noreply.github.com>
2023-12-04 17:48:51 +0800

    testing

--- src/components/beranda/beranda.jsx
+++ src/components/beranda/beranda.jsx
@@ -1,96 +1,58 @@
-import React from 'react';
-import './beranda.css';
+import React, { useState } from "react";
+import "./beranda.css";
 
 const SocialMediaCard = () => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
+  const handleToggleComments = () => {
+    setShowComments(!showComments);
+  };
+
+  const handleAddComment = (comment) => {
+    setComments([...comments, comment]);
+  };
+
   return (
-    <div>
-      <div className="container">
-        <div className="card-container">
-        <div className="side-nav">
-        <a href="#home">
-          <img src="/images/home.jpg" alt="Home" className="nav-icon" />Home</a>
-        <a href="#explore">
-          <img src="/images/explore.jpg" alt="Explore" className="nav-icon" />Explore</a>
-        <a href="#saved">
-          <img src="/images/save.jpg" alt="Saved" className="nav-icon" />Saved</a>
-        <a href="#messages"><img src="/images/chat.jpg" alt="Messages" className="nav-icon" />Messages</a></div>
-
-          <div className="custom-size-card">
-            <h2>Question</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
-          </div>
-          <div className="side-nav">
-          <p>Contacts</p>
+    <div className="post">
+      <img
+        src="https://placekitten.com/300/200" // Ganti dengan URL gambar post
+        alt="Post"
+        className="post-image"
+      />
+      <div className="post-icons">
+        <span className={`icon ${isLiked ? "liked" : ""}`} onClick={handleLike}>
+          ❤️
+        </span>
+        <span className="icon" onClick={handleToggleComments}>
+          💬
+        </span>
+        <span className="icon">💾</span>
+      </div>
+      {showComments && (
+        <div className="comments">
           <ul>
-            <li>
-              <button className="nav-button">
-                <img src="/images/comment.jpg" alt="Beranda" className="nav-icon" />
-                Alma</button>
-            </li>
-            <li>
-              <button className="nav-button">
-              <img src="/images/comment.jpg" alt="Beranda" className="nav-icon" />
-                Felix</button>
-            </li>
-            <li>
-              <button className="nav-button create-button">
-              <img src="/images/comment.jpg" alt="Beranda" className="nav-icon" />
-                Dims</button>
-            </li>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment}</li>
+            ))}
           </ul>
+          <input
+            type="text"
+            placeholder="Add a comment..."
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleAddComment(e.target.value);
+                e.target.value = "";
+              }
+            }}
+          />
         </div>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="card-container">
-        <div className="side-nav">
-          <p>Stories</p>
-          <a href="#home">
-            <img src="/images/story.jpg" alt="pengguna" className="nav-icon1" />maulanafzr</a>
-          <a href="#explore">
-            <img src="/images/story.jpg" alt="pengguna" className="nav-icon1" />felix dian</a>
-          <a href="#saved">
-            <img src="/images/story.jpg" alt="pengguna" className="nav-icon1" />almadtha</a>
-          <a href="#messages">
-            <img src="/images/story.jpg" alt="pengguna" className="nav-icon1" />dimsali</a>
-          <a href="#messages">
-            <img src="/images/story.jpg" alt="pengguna" className="nav-icon1" />hmif.itk</a>
-          <a href="#messages">
-            <img src="/images/story.jpg" alt="pengguna" className="nav-icon1" />km_itk</a>
-        </div>
-
-        <div className="instagram-post">
-            <div className="user-info">
-              <img src="/images/profile.jpg" alt="Profile" className="profile-image" />
-              <span className="username">felix</span>
-            </div>
-            <img src="https://placekitten.com/600/300" alt="Post" className="post-image" />
-            <div className="actions">
-              <div className="like-share">
-                <button className="action-button">Like</button>
-                <button className="action-button">Share</button>
-              </div>
-              <div className="comments">
-                <div className="comment">
-                  <span className="comment-username">maulanafzr:</span> lucunya kucingmu lix
-                </div>
-                <div className="comment">
-                  <span className="comment-username">dims:</span> keren
-                </div>
-                <div className="comment">
-                  <span className="comment-username">alma:</span> imutnya 
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-          <div className="side-nav">
-            <p>Recommended</p>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
