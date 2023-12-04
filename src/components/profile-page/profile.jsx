9116002a2238d01302bc70cc723678f56606a78a
DimasAliA <143428324+DimasAliA@users.noreply.github.com>
2023-12-04 23:49:04 +0800

    Update profile.jsx

--- src/components/profile-page/profile.jsx
+++ src/components/profile-page/profile.jsx
@@ -1,19 +1,23 @@
-import React,{ useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from './Navigation';
 import ProfileForm from './ProfileForm';
 import Footer from './Footer';
 
 const Profile = () => {
-    const [profileImage, setProfileImage] = useState('/images/wibu.jpg');
+    const [profileImage, setProfileImage] = useState('');
+
+    useEffect(() => {
+        const storedImage = localStorage.getItem('profileImage');
+        if (storedImage) {
+            setProfileImage(storedImage);
+        }
+    }, []);
     return (
-      <>
-        <Navigation profileImage={profileImage} />
-        <ProfileForm profileImage={profileImage} setProfileImage={setProfileImage} />
-        <Footer />
-      </>
+        <>
+            <Navigation profileImage={profileImage} />
+            <ProfileForm profileImage={profileImage} setProfileImage={setProfileImage} />
+            <Footer />
+        </>
     );
-  };
-  
-
+};
 export default Profile;
-
