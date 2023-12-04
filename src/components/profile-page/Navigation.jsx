f86551a524ef79df6dec3215b230f6bb91959b01
DimasAliA <143428324+DimasAliA@users.noreply.github.com>
2023-12-04 23:49:33 +0800

    Update Navigation.jsx

--- src/components/profile-page/Navigation.jsx
+++ src/components/profile-page/Navigation.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import './nav.css';
 
 function Navigation({ profileImage }) {
-  return (
-    <nav className='nav-profile-page'>
-      <ul>
-        <li><button>Beranda</button></li>
-        <li><button>Teman</button></li>
-        <li><button>Buat</button></li>
-      </ul>
-      <div className="search-profile-container">
-        <input className='nav-search' type="search" placeholder="Cari..." />
-        <div className="profile-circle">
-          <img src={profileImage} alt="Profile" />
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className='nav-profile-page'>
+            <ul>
+                <li><button>Beranda</button></li>
+                <li><button>Teman</button></li>
+                <li><button>Buat</button></li>
+            </ul>
+            <div className="search-profile-container">
+                <input className='nav-search' type="search" placeholder="Cari..." />
+                <div className="profile-circle">
+                    <img src={profileImage} alt="Profile" />
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
